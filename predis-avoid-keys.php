7af465c1bd40ec0c8c107e0765744b1b52c313db
Gabriel Machado <gabriel.ms1@hotmail.com>
2017-12-13 11:29:35 -0200

    Create predis-avoid-keys.php

---
+++ predis-avoid-keys.php
@@ -0,0 +1,24 @@
+<?php
+
+// Redis KEYS command can burn your Redis in production
+// Redis SCAN is safer but is harder to use...
+// Until now!
+
+use Predis\Client;
+
+$redisClient = new Predis\Client();
+
+// Use this pattern to match Redis keys
+$pattern = '*';
+
+// KEYS way (avoid this)
+$keys = $redisClient->keys($pattern);
+
+// SCAN way (just do it)
+$iterator = 0;
+do {
+    list($iterator, $keys) = Redis::scan($iterator, 'match', $pattern);
+    if ($keys) {
+        Redis::del($keys);
+    }
+} while ($iterator);
